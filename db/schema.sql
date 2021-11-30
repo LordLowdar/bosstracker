5be951035df5317fc01ff3cb21ac7e4a782f9a1a
Julian Williams <julian.th.williams@gmail.com>
2021-11-29 22:41:14 -0800

    I still havent learned to commit more

--- db/schema.sql
+++ db/schema.sql
@@ -18,8 +18,8 @@ REFERENCES department(id)
 
 CREATE TABLE employee (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
 role_id INT,
 manager_id INT,
 FOREIGN KEY(role_id) 
